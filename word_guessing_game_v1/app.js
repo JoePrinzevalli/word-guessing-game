44e3ecec737bbb428d0c531ded35b46fbf6924ef
JoePrinzevalli <joeprinzevalli123@gmail.com>
2022-02-05 20:22:50 -0500

    how to fix double 'show' class

--- word_guessing_game_v1/app.js
+++ word_guessing_game_v1/app.js
@@ -1,10 +1,20 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 let missed = 0;
-const startOverlay = document.querySelector('.btn_reset');
-let heart = document.querySelector('.tries');
+const startOverlay = document.querySelector('a');
+let heart = document.querySelectorAll('.tries img');
 const win = document.getElementsByClassName('win')
 const lose = document.getElementsByClassName('lose')
+const letter = document.getElementsByClassName('letter');
+const show = document.getElementsByClassName('show');
+let startScreen = document.querySelector('.start');
+const title = document.querySelector('.title');
+// use this as refesh button
+const refreshButton = document.createElement('button');
+
+
+
+
 
 //This changes the screen overlay to the game screen from the start screen
 addEventListener('click', (e) => {              // do i need to add an element before addEventListener, like 'startOverlay.addEventListener
@@ -18,7 +28,7 @@ const phrases = [
         'tree house',
         'new york',
         'video games',
-        'chocolate chip cookies',
+        'zeus',
         'iron man'
 ];
 //This function gets a random string from the array 'phrases', and splits the phrase and returns each letter indivually in an array.
@@ -57,7 +67,7 @@ const checkLetter = button => {
     let match = null;
     for (let i = 0; i < checkLetter.length; i++) {
         if ( button.textContent === checkLetter[i].textContent ) {
-            checkLetter[i].className += ' show';
+            checkLetter[i].className += ' show'; //why does this 'show' class display twice whe inspecting the <li> child of div #phrase
             match = checkLetter.textContent;
         } 
     }
@@ -67,34 +77,58 @@ const checkLetter = button => {
 
 qwerty.addEventListener('click', (e) => {
     let buttonClicked = e.target;
-    console.log(buttonClicked)
 
     if ( buttonClicked.tagName === 'BUTTON' ) {
         buttonClicked.className += ' chosen';
         buttonClicked.disabled = true;
-    }
-    checkLetter(buttonClicked);;
+        checkLetter(buttonClicked);
 
-    const letterFound = checkLetter(buttonClicked)
-    console.log(letterFound)
-    if (letterFound === null) {
-        missed++;
-        console.log(missed);
-        heart = document.getElementsByClassName('tries').src="images/lostHeart.png";
+        const letterFound = checkLetter(buttonClicked)
+        if (letterFound === null) {
+            heart[missed].src="images/lostHeart.png";
+            missed++;
+         }
     }
+    
     checkWin();
 })
 
+// this function checks to see if user 'won' or 'lost', then displays the appororiate screen
 const checkWin = () => {
-    start.className += ' win';
-    start.className += ' lose';
-    if (document.getElementsByClassName('letter').length === document.getElementsByClassName('show').length) {
-        style.display = win;
-    } else if (missed >= 5){
-        style.display = lose;
-
+    if (letter.length === show.length) {
+        startScreen.className += ' win';
+        startScreen.style.display = 'flex';
+        title.innerHTML = `<h2>You Won! Congratulations!</h2>`;
+        startOverlay.innerHTML = `<p>Try to beat your score!</p>`;
+        startScreen.append(refreshButton);
+        refreshButton.innerHTML = 'Click here to play again';
+        refreshButton.className = 'refresh-win';
+    } else if (missed >= 5) {
+        startScreen.className += ' lose';
+        startScreen.style.display = 'flex';
+        title.innerHTML = `<h2>Oh No, You lost!</h2>`;
+        startOverlay.innerHTML = `<p>Try Again!</p>`;
+        startScreen.append(refreshButton);
+        refreshButton.innerHTML = 'Click here to play again';
+        refreshButton.className = 'refresh-lose';
     }
 }
+
+// When user clicks button, page refrehses allowing user to play the game again
+refreshButton.addEventListener('click', reloadPage = () => {
+    reload = location.reload();
+    return reload;
+} );
+
+// const playAgainButton = () => {
+//     if (checkWin()) {
+//         refreshButton.innerHTML = 'Press Here To Play !';
+//         refreshButton.setAttribute = ('href', 'file:///Users/joeprinzevalli/Desktop/Team%20Treehouse/word-guessing-game/word_guessing_game_v1/index.html') 
+//     }
+// };
+// refreshButton.addEventListener = ('click', playAgainButton());
+
+
 
 
 
